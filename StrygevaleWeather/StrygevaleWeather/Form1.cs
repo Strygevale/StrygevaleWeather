995866c89c2a9716f8b3dfa998e1d2fc4aa65c92
Andrey Schmider <shmider272244@mail.ru>
2019-09-08 18:05:35 +0600

    first commit

---
+++ StrygevaleWeather/StrygevaleWeather/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace StrygevaleWeather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            WebRequest request = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=Omsk&APPID=d9ef62e1a9cdca288189217e245bf78d");
+
+            request.Method = "POST";
+
+            request.ContentType = "application/x-www-urlencoded";
+
+            WebResponse response = await request.GetResponseAsync();
+
+            string answer = string.Empty;
+
+            using (Stream s = response.GetResponseStream())
+            {
+
+                using (StreamReader reader = new StreamReader(s))
+                {
+                    answer = await reader.ReadToEndAsync();
+                }
+            }
+            response.Close();
+
+            richTextBox1.Text = answer;
+        }
+
+      
+    }
+}
